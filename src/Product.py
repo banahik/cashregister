034e9c2251c1cc2300b5ad9537a91cbc584ed5a7
Gordejchik Aleksandr <banahik@gmail.com>
2024-03-11 16:40:09 +0300

    add data load from db

---
+++ src/Product.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+
+
+class Product:
+
+    product_list  = []
+
+    def __init__(self, name, name_ru, price, image_data):
+        self.name = name
+        self.name_ru = name_ru
+        self.price = price
+        self.image_data = image_data
+
+        if hasattr(self, 'product_list'):
+            Product.product_list.append(self)
+        else:
+            Product.product_list = [self]
+
+    @classmethod
+    def load_data(self):
+        """
+        Загрузить данные из базы данных
+        """
+
+        self.connection = sqlite3.connect('database.db')
+        self.cursor = self.connection.cursor()
+
+        self.cursor.execute(f"""SELECT * FROM Products;""")
+        data = self.cursor.fetchall()
+
+        self.connection.commit()
+        self.connection.close()
+
+
+        for product in data:
+            Product(name=product[0],
+                    name_ru=product[1],
+                    price=product[2],
+                    image_data=product[3])
+
+
