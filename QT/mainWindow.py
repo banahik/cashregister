86a5ecdfb0685472386ff1c403fab7d8d5614763
Gordejchik Aleksandr <banahik@gmail.com>
2024-03-11 18:42:44 +0300

    product list work

--- QT/mainWindow.py
+++ QT/mainWindow.py
@@ -56,6 +56,9 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout.addWidget(self.bt_best_product)
 
+        self.verticalLayout_3 = QVBoxLayout()
+        self.verticalLayout_3.setSpacing(0)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
         self.label_best_product = QLabel(self.centralwidget)
         self.label_best_product.setObjectName(u"label_best_product")
         font2 = QFont()
@@ -65,7 +68,15 @@ class Ui_MainWindow(object):
         self.label_best_product.setFont(font2)
         self.label_best_product.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignTop)
 
-        self.horizontalLayout.addWidget(self.label_best_product)
+        self.verticalLayout_3.addWidget(self.label_best_product)
+
+        self.bt_add_product = QPushButton(self.centralwidget)
+        self.bt_add_product.setObjectName(u"bt_add_product")
+
+        self.verticalLayout_3.addWidget(self.bt_add_product)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout_3)
 
 
         self.verticalLayout_2.addLayout(self.horizontalLayout)
@@ -139,6 +150,7 @@ class Ui_MainWindow(object):
         self.actionsitem_panel.setText(QCoreApplication.translate("MainWindow", u"system panel", None))
         self.bt_best_product.setText("")
         self.label_best_product.setText("")
+        self.bt_add_product.setText(QCoreApplication.translate("MainWindow", u"\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"\u041f\u043e\u0438\u0441\u043a", None))
         self.bt_buy.setText(QCoreApplication.translate("MainWindow", u"\u041e\u043f\u043b\u0430\u0442\u0438\u0442\u044c", None))
         self.menusattings.setTitle(QCoreApplication.translate("MainWindow", u"Settings", None))
--- cashregister.py
+++ cashregister.py
@@ -13,6 +13,10 @@ import random
 class MainWindow(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
 
     product_mass = 0
+    product_name = ''
+    product_sum = 0
+
+    All_sum = 0
 
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -23,6 +27,9 @@ class MainWindow(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
         self.table_products.insertColumn(0)
         self.table_products.setHorizontalHeaderLabels(['Товар'])
         self.table_products.horizontalHeader().hide()
+
+        self.bt_add_product.clicked.connect(self.add_product_to_buy_lust)
+        self.bt_buy.clicked.connect(self.buy)
 
 
 
@@ -53,21 +60,53 @@ class MainWindow(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
             self.table_products.setCellWidget(0,0,bt)
             self.table_products.setItem(0, 1, QtWidgets.QTableWidgetItem(str(one_product.name_ru)))
 
+    
+
 
     def clicked_product(self):
+        """
+        Отобразить продукт на превью
+        """
         data = Product.get_product_by_name(self.sender().objectName())
-        print(data.name)
         pix = QPixmap()
         pix.loadFromData(data.image_data) 
         self.bt_best_product.setIcon(pix)
         self.bt_best_product.setIconSize(QtCore.QSize(200,200))
 
         self.product_mass = random.random().__round__(3)
+        self.product_name = data.name_ru
+        self.product_sum = round(self.product_mass*data.price,3)
 
-        label = f"""Товар: {data.name_ru}\nМасса: {self.product_mass}\nЦена: {data.price}\nСумма: {round(self.product_mass*data.price,3)}"""
+        label = f"""Товар: {data.name_ru}\nМасса: {self.product_mass}\nЦена: {data.price}\nСумма: { self.product_sum}"""
 
         self.label_best_product.setText(label)
     
+
+
+
+    numbers_iterator = iter(range(1,999))
+    
+
+    def add_product_to_buy_lust(self):
+        """
+        Добавление продукта с список покупок
+        """
+        label = str.ljust(str(next(self.numbers_iterator)),3,'.')
+        label += str.ljust(str(self.product_name),25,'_')
+        label += str(self.product_sum)
+        self.list_products_buy.addItem(label)
+
+        self.All_sum += self.product_sum
+
+
+    def buy(self):
+        """
+        Кнопка купить
+        """
+        print(f'Куплено продуктов на {self.All_sum}')
+        self.All_sum = 0
+        self.list_products_buy.clear()
+        self.numbers_iterator = iter(range(1,999))
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
